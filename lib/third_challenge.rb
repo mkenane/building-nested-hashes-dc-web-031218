7bf0e2c1d64d9a4b43119306827dfa0fb403aaa3
Mor Kenane <morkenan@hotmail.com>
2018-02-23 21:07:36 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,16 +11,16 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => [
         {:name => "Benvolio",
-         :age => "17"
+         :age => "17",
          :attitude => "worried"
         },
         {:name => "Mercutio",
-         :age => "18"
+         :age => "18",
          :attitude => "hot-headed"
         }
         ]
@@ -36,16 +36,16 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => [
          {:name => "Steven",
-         :age => "30"
+         :age => "30",
          :attitude => "confused"
         },
         {:name => "Nurse",
-         :age => "44"
+         :age => "44",
          :attitude => "worried"
         }]
    }
